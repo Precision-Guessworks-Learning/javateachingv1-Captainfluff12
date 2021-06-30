24c6f8defa13cea3ea5690d975a3af16fff5cfb0
Captainfluff12 <Oranginthetheatre@gmail.com>
2021-06-30 19:01:07 -0400

    Add exercise 2_2

---
+++ basics/L2_datatypes/Exercise2_2.java
@@ -0,0 +1,12 @@
+package basics.L2_datatypes;
+
+public class Exercise2_2 {
+    public static void main(String[] args) {
+       
+       
+       
+    String name = "Secretary Bird" + ", Cardinal" + ", Ostrich";
+    System.out.println(name + " are cool birds.");
+
+    }
+}
