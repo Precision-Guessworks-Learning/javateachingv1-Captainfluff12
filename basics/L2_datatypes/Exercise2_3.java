fa1efc8a6fbcd7ab21d9399909e96ad1f6b9c7fd
Captainfluff12 <Oranginthetheatre@gmail.com>
2021-06-30 19:15:29 -0400

    Added Exercise 2_3

---
+++ basics/L2_datatypes/Exercise2_3.java
@@ -0,0 +1,18 @@
+package basics.L2_datatypes;
+
+public class Exercise2_3 {
+    public static void main(String[] args) {
+
+    boolean APOC = false;
+    System.out.println("Is it the apocalapse?");
+    System.out.println(APOC);
+
+    System.out.println("WAIT... is that Killer Space Worms?");
+    boolean space = true;
+    System.out.println("Is it the apocalapse now?");
+    System.out.println(space);
+
+
+
+    }
+}
