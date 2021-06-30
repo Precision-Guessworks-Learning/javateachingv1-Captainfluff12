1855e38f3debf25255d6d22d874f595f8b5c52e0
Captainfluff12 <Oranginthetheatre@gmail.com>
2021-06-30 19:31:51 -0400

    Added Exercise 2_4

---
+++ basics/L2_datatypes/Exercise2_4.java
@@ -0,0 +1,26 @@
+package basics.L2_datatypes;
+
+public class Exercise2_4 {
+    public static void main(String[] args) {
+
+    int Alli = 3;
+    double Allitwo = 103.26;
+    System.out.println("No one wants half an alligator, come down to Dimmadome Emporium and get your whole alligator today!");
+    System.out.println("Whole Alligators are "+ "$"+ Allitwo);
+    System.out.println("I only have "+ Alli + " Left in stock");
+    
+    double Allithree = 3.26;
+    
+    double Allifour = 96.74;
+
+    System.out.println("Right now my Alligators for " + Allitwo);
+    
+    System.out.println("uh oh, now they are only " + Allithree);
+
+    System.out.println("OKAY, OKAY, OKAY " + "take all of my Alligators and here's " + Allifour + " of my money!");
+
+
+
+
+    }
+}
