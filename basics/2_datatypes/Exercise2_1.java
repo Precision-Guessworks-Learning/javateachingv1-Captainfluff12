2c32e3751bc300f4f2d4cc8efff4137892dd560a
Captainfluff12 <Oranginthetheatre@gmail.com>
2021-06-30 18:20:46 -0400

    Exercise2_1

---
+++ basics/2_datatypes/Exercise2_1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class Exercise2_1{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        int Alli = 3;
+        double Allitwo = 103.26;
+        System.out.println("No one wants half an alligator, come down to Dimmadome Emporium and get your whole alligator today!");
+        System.out.println("Whole Alligators are "+ "$"+ Allitwo);
+        System.out.println("I only have "+ Alli + " Left in stock");
+       
+        String name = "Secretary Bird" + ", Cardinal" + ", Blue Jay";
+
+        System.out.println(name);
+
+        }
+    }
